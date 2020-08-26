4596043ffdbf5cd0cf00f2d4cb41baa3b3bd1b11
ritvij14 <ritvij2001@gmail.com>
2020-08-26 22:23:23 +0530

    Made project structure

--- app/src/main/java/com/example/event_management/activities/MainActivity.java
+++ app/src/main/java/com/example/event_management/activities/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.event_management;
+package com.example.event_management.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.event_management.R;
 
 public class MainActivity extends AppCompatActivity {
 
