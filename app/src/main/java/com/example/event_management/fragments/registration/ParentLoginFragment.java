0038ebe640bbb46fcacd996ee4ccd8d924dc457e
ritvij14 <ritvij2001@gmail.com>
2020-09-05 15:21:50 +0530

    ParentRegistrationFragment part 1 for logging in is complete.

--- app/src/main/java/com/example/event_management/fragments/registration/ParentLoginFragment.java
+++ app/src/main/java/com/example/event_management/fragments/registration/ParentLoginFragment.java
@@ -13,18 +13,34 @@ import android.view.ViewGroup;
 import com.example.event_management.R;
 import com.example.event_management.databinding.FragmentParentLoginBinding;
 
+import java.util.Objects;
+
 public class ParentLoginFragment extends Fragment {
 
     FragmentParentLoginBinding parentLoginBinding;
+    ParentRegistrationFragment parentRegistrationFragment;
     public ParentLoginFragment() {
         // Required empty public constructor
     }
 
     @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        parentLoginBinding = FragmentParentLoginBinding.inflate(inflater, container, false);
+        parentLoginBinding = FragmentParentLoginBinding
+            .inflate(inflater, container, false);
+        parentRegistrationFragment = new ParentRegistrationFragment();
+
+        parentLoginBinding.loginToRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Objects.requireNonNull(getActivity())
+                    .getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.registration_frame_layout, parentRegistrationFragment)
+                    .commit();
+            }
+        });
         return parentLoginBinding.getRoot();
     }
 
--- app/src/main/java/com/example/event_management/fragments/registration/ParentRegistrationFragment.java
+++ app/src/main/java/com/example/event_management/fragments/registration/ParentRegistrationFragment.java
@@ -9,58 +9,41 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.event_management.R;
+import com.example.event_management.databinding.FragmentParentRegistrationBinding;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ParentRegistrationFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.Objects;
+
 public class ParentRegistrationFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
+    FragmentParentRegistrationBinding registrationBinding;
+    ParentLoginFragment parentLoginFragment;
     public ParentRegistrationFragment() {
         // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ParentRegistrationFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ParentRegistrationFragment newInstance(String param1, String param2) {
-        ParentRegistrationFragment fragment = new ParentRegistrationFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_parent_registration, container, false);
+        registrationBinding = FragmentParentRegistrationBinding
+            .inflate(inflater, container, false);
+        parentLoginFragment = new ParentLoginFragment();
+
+        registrationBinding.registerToLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Objects.requireNonNull(getActivity())
+                    .getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.registration_frame_layout, parentLoginFragment)
+                    .commit();
+            }
+        });
+        return registrationBinding.getRoot();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        registrationBinding = null;
     }
 }
