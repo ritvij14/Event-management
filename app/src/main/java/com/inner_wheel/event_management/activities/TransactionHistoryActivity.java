ace977ad385697f78458eae5e6029e8c19fb030f
Ritvij Kumar Sharma <ritvij2001@gmail.com>
2020-11-01 01:34:53 +0530

    added load toast to transactions history page

--- app/src/main/java/com/inner_wheel/event_management/activities/TransactionHistoryActivity.java
+++ app/src/main/java/com/inner_wheel/event_management/activities/TransactionHistoryActivity.java
@@ -6,14 +6,19 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.transition.TransitionManager;
-import android.view.View;
+import android.widget.Toast;
 
+import com.inner_wheel.event_management.R;
 import com.inner_wheel.event_management.adapters.TransactionHistoryAdapter;
 import com.inner_wheel.event_management.api.RetrofitClient;
 import com.inner_wheel.event_management.api.models.TransactionHistoryResponse;
 import com.inner_wheel.event_management.api.models.TransactionItem;
 import com.inner_wheel.event_management.databinding.ActivityTransactionHistoryBinding;
 import com.inner_wheel.event_management.utils.SharedPrefs;
+
+import net.steamcrafted.loadtoast.LoadToast;
+
+import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
 
@@ -26,12 +31,17 @@ public class TransactionHistoryActivity extends AppCompatActivity {
     ActivityTransactionHistoryBinding historyBinding;
     SharedPrefs sharedPrefs;
     TransactionHistoryAdapter historyAdapter;
+    LoadToast lt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         historyBinding = ActivityTransactionHistoryBinding.inflate(getLayoutInflater());
         setContentView(historyBinding.getRoot());
         sharedPrefs = new SharedPrefs(this);
+        lt = new LoadToast(this);
+        lt.setText("Loading...");
+        lt.setTranslationY(100).setBorderColor(getResources().getColor(R.color.color_accent));
+        lt.show();
         TransitionManager.beginDelayedTransition(historyBinding.transactionHistoryRv);
 
         fetchTransactions();
@@ -43,19 +53,27 @@ public class TransactionHistoryActivity extends AppCompatActivity {
         Call<TransactionHistoryResponse> call = RetrofitClient.getClient().getTransactionHistory(sharedPrefs.getToken());
         call.enqueue(new Callback<TransactionHistoryResponse>() {
             @Override
-            public void onResponse(Call<TransactionHistoryResponse> call, Response<TransactionHistoryResponse> response) {
+            public void onResponse(@NotNull Call<TransactionHistoryResponse> call, @NotNull Response<TransactionHistoryResponse> response) {
                 TransactionHistoryResponse res = response.body();
                 if (res != null) {
                     if (res.isSuccess()) {
+                        lt.success();
+                        lt.hide();
                         List<TransactionItem> transactionItemList= res.getTransactionItems();
                         createHistoryRecyclerView(transactionItemList);
                     }
+                } else {
+                    lt.error();
+                    lt.hide();
+                    Toast.makeText(TransactionHistoryActivity.this, "No past transactions found at the moment", Toast.LENGTH_SHORT).show();
                 }
             }
 
             @Override
-            public void onFailure(Call<TransactionHistoryResponse> call, Throwable t) {
-
+            public void onFailure(@NotNull Call<TransactionHistoryResponse> call, @NotNull Throwable t) {
+                lt.error();
+                lt.hide();
+                Toast.makeText(TransactionHistoryActivity.this, "Error!", Toast.LENGTH_SHORT).show();
             }
         });
     }
